2be178aabf03c94e2d4eea9402ef04df9c1b6c29
Dan Weston <dweston@8ball.local>
2013-05-18 12:50:01 -0700

    Initial Commit

---
+++ 7MinuteWorkout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  7MinuteWorkout
+//
+//  Created by Dan Weston on 5/18/13.
+//  Copyright (c) 2013 Dan Weston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 7MinuteWorkout/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  7MinuteWorkout
+//
+//  Created by Dan Weston on 5/18/13.
+//  Copyright (c) 2013 Dan Weston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
